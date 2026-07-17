5d3032d6f531916d802f04116f5fea969944e3c1
P Vignesh <p.vignesh2909@gmail.com>
2026-07-17 11:18:06 +0530

    CINEVERSE

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
 
 interface User {
   email: string;
