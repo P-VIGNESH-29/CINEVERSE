990caa22abdd74762bf2cc80170f5bc5959b4169
P Vignesh <p.vignesh2909@gmail.com>
2026-07-17 11:34:22 +0530

    CINEVERSE

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store/store.tsx'
 import { ThemeProvider } from './context/ThemeContext'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <HashRouter>
+
       <ThemeProvider>
         <App />
       </ThemeProvider>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
